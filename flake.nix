771628eea2db4b7a1d7ce0785915cbe1b15ab70f
Jonathan Davies <jpds@protonmail.com>
2025-11-22 12:10:40 +0000

    nix: Add cargo2nix integration

---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+  inputs = {
+    cargo2nix.url = "github:cargo2nix/cargo2nix/release-0.12";
+    flake-utils.follows = "cargo2nix/flake-utils";
+    nixpkgs.follows = "cargo2nix/nixpkgs";
+  };
+
+  outputs =
+    inputs:
+    with inputs;
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ cargo2nix.overlays.default ];
+        };
+
+        rustPkgs = pkgs.rustBuilder.makePackageSet {
+          rustVersion = "latest";
+          packageFun = import ./Cargo.nix;
+        };
+
+      in
+      rec {
+        packages = {
+          prometheus-sd-nexthop = (rustPkgs.workspace.prometheus-sd-nexthop { });
+          default = packages.prometheus-sd-nexthop;
+        };
+      }
+    );
+}
