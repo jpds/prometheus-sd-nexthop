bb5fee61279e8097d6a0deabd8a4517c9088f7a9
Jonathan Davies <jpds@protonmail.com>
2025-12-03 15:44:40 +0000

    build.rs: Add script to read short hash from Git repository

---
+++ build.rs
@@ -0,0 +1,28 @@
+use std::error::Error;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let repo = gix::discover(".").ok();
+
+    let git_hash = match repo {
+        Some(repo) => repo
+            .head()
+            .ok()
+            .and_then(|mut head| head.peel_to_commit().ok())
+            .and_then(|commit| commit.short_id().ok())
+            .map(|prefix| prefix.to_string()),
+        None => None,
+    };
+
+    // If no Git repository available, read REV from Nix definition
+    let git_hash = git_hash.clone().unwrap_or_else(|| {
+        option_env!("PROMETHEUS_SD_NEXTHOP_NIX_BUILD_REV")
+            .map(|v| v.to_string())
+            .unwrap_or_else(|| "unknown".to_string())
+    });
+
+    let git_hash_short = git_hash.get(0..8).unwrap_or_else(|| &git_hash);
+
+    println!("cargo:rustc-env=BUILD_GIT_HASH={}", git_hash_short);
+
+    Ok(())
+}
